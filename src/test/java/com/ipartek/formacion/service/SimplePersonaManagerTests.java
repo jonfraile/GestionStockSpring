13c2ee0d98b0f307a073f33c70668341fa1eeb8f
jonfaile <jonfraile82@gmail.com>
2016-09-13 13:30:36 +0200

    ok

--- src/test/java/com/ipartek/formacion/service/SimplePersonaManagerTests.java
+++ src/test/java/com/ipartek/formacion/service/SimplePersonaManagerTests.java
@@ -1,9 +1,6 @@
 package com.ipartek.formacion.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +9,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.ipartek.formacion.domain.Persona;
-import com.ipartek.formacion.domain.Product;
 
 public class SimplePersonaManagerTests {
 
 	private SimplePersonaManager personaManager;
 
 	private List<Persona> personas;
+
+	private static String NOMBRE_UNO = "Manolo";
+	private static String NOMBRE_DOS = "Manolito";
 
 	@Before
 	public void setUp() throws Exception {
@@ -39,68 +38,9 @@ public class SimplePersonaManagerTests {
 	}
 
 	@Test
-	public void testGetPersonasWithNoProducts() {
+	public void testGetPersonasWithNoPersonass() {
 		this.personaManager = new SimplePersonaManager();
 		assertNull(this.personaManager.getAll());
 	}
 
-	@Test
-	public void testGetProducts() {
-		final List<Product> products = this.productManager.getProducts();
-		assertNotNull(products);
-		assertEquals(PRODUCT_COUNT, this.productManager.getProducts().size());
-
-		Product product = products.get(0);
-		assertEquals(CHAIR_DESCRIPTION, product.getDescription());
-		assertEquals(CHAIR_PRICE, product.getPrice());
-
-		product = products.get(1);
-		assertEquals(TABLE_DESCRIPTION, product.getDescription());
-		assertEquals(TABLE_PRICE, product.getPrice());
-	}
-
-	@Test
-	public void testIncreasePriceWithNullListOfProducts() {
-		try {
-			this.productManager = new SimpleProductManager();
-			this.productManager.increasePrice(POSITIVE_PRICE_INCREASE);
-		} catch (final NullPointerException ex) {
-			fail("Products list is null.");
-		}
-	}
-
-	@Test
-	public void testIncreasePriceWithEmptyListOfProducts() {
-		try {
-			this.productManager = new SimpleProductManager();
-			this.productManager.setProducts(new ArrayList<Product>());
-			this.productManager.increasePrice(POSITIVE_PRICE_INCREASE);
-		} catch (final Exception ex) {
-			fail("Products list is empty.");
-		}
-	}
-
-	@Test
-	public void testIncreasePriceWithPositivePercentage() {
-		this.productManager.increasePrice(POSITIVE_PRICE_INCREASE);
-		final double expectedChairPriceWithIncrease = 22.55;
-		final double expectedTablePriceWithIncrease = 165.11;
-
-		final List<Product> products = this.productManager.getProducts();
-		Product product = products.get(0);
-		assertEquals(expectedChairPriceWithIncrease, product.getPrice(), 0);
-
-		product = products.get(1);
-		assertEquals(expectedTablePriceWithIncrease, product.getPrice(), 0);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testIncreasePriceWithIllegalArgumentNegative() {
-		this.productManager.increasePrice(NEGATIVE_PRICE_INCREASE);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testIncreasePriceWithIllegalArgumentOver() {
-		this.productManager.increasePrice(OVER_PRICE_INCREASE);
-	}
 }
