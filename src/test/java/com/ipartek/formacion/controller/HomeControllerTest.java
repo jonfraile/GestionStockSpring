fd8c9300346b5a3626648cc3d52f2da6dccfe685
jonfaile <jonfraile82@gmail.com>
2016-09-14 09:26:22 +0200

    Pendiente Mocks

--- src/test/java/com/ipartek/formacion/controller/HomeControllerTest.java
+++ src/test/java/com/ipartek/formacion/controller/HomeControllerTest.java
@@ -2,30 +2,11 @@ package com.ipartek.formacion.controller;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HomeControllerTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	// TODO REQUEST MOCK
 	@Test
 	public void testHome() {
 		final HomeController hc = new HomeController();
